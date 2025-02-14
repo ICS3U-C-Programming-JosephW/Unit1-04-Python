ca5b3f4380956bc0f86f8ba954dba90078479f1a
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-02-14 18:07:17 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: Feb. 14, 2025
+# This program does basic math.
+
+import math
+
+
+def main():
+
+    # Stores a table of tuples that include the string and literal version of the expressions.
+    t = [
+        ("2 + 3", 2 + 3),  # Addition.
+        ("5 - 4", 5 - 4),  # Subtraction.
+        ("7 * 6", 7 * 6),  # Multiplication.
+        ("8 / 3", 8 / 3),  # Division.
+        ("3 ** 2", 3**2),  # Exponentiation (Powers).
+        ("\u221a 4", math.sqrt(4)),  # Square Root.
+        ("99 % 8", 99 % 8),  # Modulo.
+    ]
+
+    # For loop to iterate the elements of the table, where i[0] and i[1] are the first and second elements of the tuple, respectively.
+    for i in t:
+        print(i[0] + " = {} ".format(i[1]))
+
+
+if __name__ == "__main__":
+    main()
